23d211f9896370bd822d59a4b035787d7f4cf26b
boringotaku <eprasad86@gmail.com>
2022-02-07 15:53:01 +0530

    added to my project

---
+++ disease.h
@@ -0,0 +1,17 @@
+class disease : public patient {
+  std::string diseaseName;
+  std::string doctor;
+  public:
+    disease(std::string dis="common cold", std::string doc="general"){
+      diseaseName = dis;
+      doctor      = doc;
+    }
+    void getInfo();
+};
+
+void disease::getInfo() {
+  patient::getInfo();
+  std::cout << diseaseName << std::endl;
+  std::cout << doctor << std::endl;
+}
+
